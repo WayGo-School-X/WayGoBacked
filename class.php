b41bd3d5c9a22ca19e0081e494aa718aa3f6fe07
Dmitry Samsonov <101480666+dima33456@users.noreply.github.com>
2023-03-11 13:31:17 +0300

    Add files via upload

--- class.php
+++ class.php
@@ -10,7 +10,7 @@
 <body>
 
 <h1>
-    var
+    Session
 </h1>
 
 <h1>
@@ -29,38 +29,41 @@
 
 <?php
 // Инициализируем сессию.
-// Если вы используете session_name("something"), не забудьте добавить это перед session_start()!
 if($_POST['submitt']) {
-    session_name("something");
+    //session_name("something");
     session_start();
-    echo ('Сьаьус - ' . 1 . '<br>');
-    echo session_id();
-}
 
+    // Taking current system Time
+    $_SESSION['start'] = time();
+
+    // Destroying session after 1 minute
+    $_SESSION['expire'] = $_SESSION['start'] + (1 * 10);
+
+    echo ('id - ' . '<br>');
+    echo session_id();
+
+}
 
 // Наконец, уничтожаем сессию.
 else if($_POST['no_submit']) {
-
     // Удаляем все переменные сессии.
     $_SESSION = array();
 
     // Если требуется уничтожить сессию, также необходимо удалить сессионные cookie.
-// Замечание: Это уничтожит сессию, а не только данные сессии!
-    echo session_id() . "<br>";
+    // Замечание: Это уничтожит сессию, а не только данные сессии!
+
     if (ini_get("session.use_cookies")) {
         $params = session_get_cookie_params();
-        setcookie(session_name("something"), '', time() - 10000000,
+        setcookie(session_name("something"), '', time() - 42000,
             $params["path"], $params["domain"],
             $params["secure"], $params["httponly"]
         );
     }
 
-
-    #session_destroy("something");
-    echo 'Сьаьус - ' . 0 . "<br>";
+    //session_destroy();
+    echo 'end' . "<br>";
     echo session_id();
 }
 ?>
-
 </body>
 </html>
